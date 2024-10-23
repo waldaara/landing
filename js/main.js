502c548d18bcd2e92a5b0bbfa98f248b9e7f2157
David Aragundy <136662251+waldaara@users.noreply.github.com>
2024-10-23 15:39:07 +0000

    tf??

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let ready = () => {
+    console.log('DOM está listo')
+}
+
+let loaded = () => {
+    console.log('Iframes e Images cargadas')
+
+    let myform = document.getElementById('form');
+
+    myform.addEventListener('submit', (eventSubmit) => {
+        eventSubmit.preventDefault();
+
+        const emailElement = document.querySelector('.form-control-lg');
+        const emailText = emailElement.value;
+
+        if (emailText.length === 0) {
+            emailElement.focus()
+        }
+
+        emailElement.animate(
+            [
+                { transform: "translateX(0)" },
+                { transform: "translateX(50px)" },
+                { transform: "translateX(-50px)" },
+                { transform: "translateX(0)" }
+            ],
+            {
+                duration: 400,
+                easing: "linear",
+            }
+        )
+    })
+}
+
+window.addEventListener("DOMContentLoaded", ready);
+window.addEventListener("load", loaded)
