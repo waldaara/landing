39fa3bc11555d950df8707048408e02fa7e5ce51
David Aragundy <136662251+waldaara@users.noreply.github.com>
2024-10-28 15:29:46 +0000

    fuego base???

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+
+const databaseURL = "https://landing-c6440-default-rtdb.firebaseio.com/data.json";
+
+let sendData = () => {
+
+    const form = document.getElementById('form');
+
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error en la solicitud: ${response.statusText}`);
+            }
+            return response.json(); // Procesa la respuesta como JSON
+        })
+        .then(result => {
+            alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+            form.reset()
+        })
+        .catch(error => {
+            alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        });
+}
+
 let ready = () => {
     console.log('DOM está listo')
 }
@@ -14,21 +50,25 @@ let loaded = () => {
         const emailText = emailElement.value;
 
         if (emailText.length === 0) {
+            emailElement.animate(
+                [
+                    { transform: "translateX(0)" },
+                    { transform: "translateX(50px)" },
+                    { transform: "translateX(-50px)" },
+                    { transform: "translateX(0)" }
+                ],
+                {
+                    duration: 400,
+                    easing: "linear",
+                }
+            )
             emailElement.focus()
+
+            return;
         }
 
-        emailElement.animate(
-            [
-                { transform: "translateX(0)" },
-                { transform: "translateX(50px)" },
-                { transform: "translateX(-50px)" },
-                { transform: "translateX(0)" }
-            ],
-            {
-                duration: 400,
-                easing: "linear",
-            }
-        )
+        sendData();
+
     })
 }
 
